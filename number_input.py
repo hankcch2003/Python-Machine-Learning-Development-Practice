37a8e89523a6ff56789451ba27fa0ddbfd0c1351
hankcch2003 <105833935+hankcch2003@users.noreply.github.com>
2025-07-11 17:34:42 +0800

    Delete number_input.py

--- number_input.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title("Number Input")
-n1 = st.number_input("Enter first Number:", value = 0)
-n2 = st.number_input("Enter Second Number:", value = 0)
-sum_result = n1 + n2
-st.write(f"The sum is : {sum_result}")
